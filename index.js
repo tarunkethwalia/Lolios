48a0b32672d3746ca04237fd63d3ff09cfbd680a
SillyZan <tarunkethwalia18@gmail.com>
2020-09-22 12:17:17 +0530

    fixed some bugs

--- index.js
+++ index.js
@@ -25,21 +25,29 @@ io.on('connection', (socket) => {
         socket.broadcast.to(user.room).emit('message', {user: 'Bot', text: `${user.name} has joined..!!`});
 
         socket.join(user.room);
+
+        io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)});
+
         callback();
     });
 
     socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id);
 
-        // console.log(user, message);
-        // console.log('Test');
-        io.to(user.room).emit('message', {user: user.name, text: message});
+        if(user){
+            io.to(user.room).emit('message', {user: user.name, text: message});
+            io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)});
+        }
 
         callback();
     });
 
     socket.on('disconnect', () => {
-        console.log('User Disconnected');
+        const user = removeUser(socket.id);
+        if(user) {
+            io.to(user.room).emit('message', {user: 'Bot' ,text: `${user.name} has left the room..!!`});
+            io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)});
+        }
     });
 });
 
